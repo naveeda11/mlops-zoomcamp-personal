90575bcac95c5ab32b2d1f24b14d8b96aada7e4b
Naveed <naveed.agboatwala11@gmail.com>
2023-06-03 21:29:00 -0700

    initial commit

--- cohorts/2023/02-experiment-tracking/homework/train.py
+++ cohorts/2023/02-experiment-tracking/homework/train.py
@@ -1,9 +1,13 @@
 import os
 import pickle
 import click
-
+import mlflow
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("exp1")
 
 
 def load_pickle(filename: str):
@@ -15,19 +19,25 @@ def load_pickle(filename: str):
 @click.option(
     "--data_path",
     default="./output",
-    help="Location where the processed NYC taxi trip data was saved"
+    help="Location where the processed NYC taxi trip data was saved",
 )
 def run_train(data_path: str):
-
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "nav")
+        mlflow.log_param("train-data-path", data_path)
+        max_depth = 10
+        mlflow.log_param("max_depth", max_depth)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=max_depth, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_train()
